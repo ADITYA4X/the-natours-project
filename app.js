09b9bd01ada92f1bda42119a9c4cfe9ce8a9b7bc
aditya <adityakunghadkar94.com>
2024-08-06 19:45:43 +0530

    Update error handling

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -16,13 +18,9 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
-
-app.use((req, res, next) => {
   // adding current time to request: define property on request object
   req.requestTime = new Date().toISOString();
+
   next();
 });
 
@@ -30,5 +28,13 @@ app.use((req, res, next) => {
 // Mounting new router on rout:
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// Handling unhandled routes: all- Get, Post, Delete and Patch:
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// Global Error handling middleware: 500-internal server error, 400-fail
+app.use(globalErrorHandler);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -3,6 +3,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -21,10 +27,17 @@ mongoose
   .then(() => {
     // console.log(con.connections);
     console.log('DB connection successful!');
-  })
-  .catch((err) => console.log('Refused to connect..', err));
+  });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
